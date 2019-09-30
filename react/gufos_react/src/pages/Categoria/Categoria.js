6a748b8c6e92feb09d9eec914547d2c2e71e7679
Raul Scapin Do Nascimento <46791749865@info.local>
2019-09-30 11:29:11 -0300

    react app e categorias

---
+++ react/gufos_react/src/pages/Categoria/Categoria.js
@@ -0,0 +1,108 @@
+import React,{Component} from 'react';
+import logo from '../../assets/img/icon-login.png'
+
+import Rodape from '../../components/Rodape/Rodape'
+
+
+class Categoria extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            lista: [
+                // {idCategoria: 1, nome: "Design"},
+                // {idCategoria: 2, nome: "Joojos"},
+                // {idCategoria: 3, nome: "Meetup"},
+            ]
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:5000/api/categorias')
+        .then(response => response.json())
+        .then(data => this.setState({ lista: data}))
+        .catch(error => console.log(error));
+    }
+
+    adicionarItem = (e) =>{
+        e.preventDefault();
+        this.setState({lista: [
+            {idCategoria: 4 ,nome: 'novacategoria'}
+        ]});
+    };
+
+    render(){
+         return (
+            <div>
+            <header className="cabecalhoPrincipal">
+              <div className="container">
+                <img src={logo} />
+      
+                <nav className="cabecalhoPrincipal-nav">
+                  Administrador
+                </nav>
+              </div>
+            </header>
+      
+            <main className="conteudoPrincipal">
+              <section className="conteudoPrincipal-cadastro">
+                <h1 className="conteudoPrincipal-cadastro-titulo">Categorias</h1>
+                <div className="container" id="conteudoPrincipal-lista">
+                  <table id="tabela-lista">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Título</th>
+                      </tr>
+                    </thead>
+      
+                    <tbody id="tabela-lista-corpo">
+                        {this.state.lista.map(element =>{
+                            return (
+                                <tr>
+                                    <td>{element.idCategoria}</td>
+                                    <td>{element.nome}</td>
+
+                                </tr>
+
+                            )
+
+                        })}
+
+                    </tbody>
+                  </table>
+                </div>
+      
+                <div className="container" id="conteudoPrincipal-cadastro">
+                  <h2 className="conteudoPrincipal-cadastro-titulo">
+                    Cadastrar Categoria
+                  </h2>
+                  <form>
+                    <div className="container">
+                      <input
+                        type="text"
+                        className="class__categoria"
+                        id="input__categoria"
+                        placeholder="tipo do evento"
+                        />
+                      <button
+                        id="btn__cadastrar"
+                        className="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro"
+                        onClick={this.adicionarItem}
+                      >
+                        Cadastrar
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </section>
+            </main>
+
+            <Rodape />
+
+          </div>
+         )
+    }
+}
+
+export default Categoria;
---
+++ react/gufos_react/src/pages/NaoEncontrado/NaoEncontrado.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class NaoEncontrado extends Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    404: errokk
+                </p>
+            </div>
+        )
+    }
+}
+
+export default NaoEncontrado;
